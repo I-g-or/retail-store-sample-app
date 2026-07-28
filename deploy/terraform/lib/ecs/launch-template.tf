b76eef33699877c8baa7862db36333b2f3f7d25d
Igor Rospasienko <igorrosp@gmail.com>
2026-07-28 20:44:23 +0300

    fix launch template

--- deploy/terraform/lib/ecs/launch-template.tf
+++ deploy/terraform/lib/ecs/launch-template.tf
@@ -23,37 +23,34 @@ sudo mkdir -p /etc/alertmanager
 sudo mkdir -p /etc/blackbox
 
 sudo aws ssm get-parameter \
-    --name "/${var.environment_name}/monitoring/prometheus.yml" \
+    --name "/dev/monitoring/prometheus.yml" \
     --with-decryption \
     --query Parameter.Value \
     --output text \
-    > /etc/prometheus/prometheus.yml
+    | sudo tee /etc/prometheus/prometheus.yml
 
 sudo chmod 644 /etc/prometheus/*.yml
 sudo chown root:root /etc/prometheus/*.yml
 
 sudo aws ssm get-parameter \
-    --name "/${var.environment_name}/monitoring/alertmanager.yml" \
+    --name "/dev/monitoring/alertmanager.yml" \
     --with-decryption \
     --query Parameter.Value \
     --output text \
-    | base64 -d \
-    > /etc/alertmanager/alertmanager.yml
+    | sudo tee /etc/alertmanager/alertmanager.yml
 
 sudo chmod 644 /etc/alertmanager/*.yml
 sudo chown root:root /etc/alertmanager/*.yml
 
 sudo aws ssm get-parameter \
-  --name "/${var.environment_name}/monitoring/blackbox.yml" \
+  --name "/dev/monitoring/blackbox.yml" \
   --with-decryption \
   --query Parameter.Value \
   --output text \
-  > /etc/blackbox/blackbox.yml
+  | sudo tee /etc/blackbox/blackbox.yml
 
 sudo chmod 644 /etc/blackbox/*.yml
 sudo chown root:root /etc/blackbox/*.yml
-
-echo "ECS user data script completed at $(date)" >> /var/log/ecs-init.log
 EOF
 )
 
